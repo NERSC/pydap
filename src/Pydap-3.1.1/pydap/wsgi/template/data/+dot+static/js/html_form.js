120652972e12eeb6cc82743c4d78907d6437ecbd
Shreyas Cholia <shreyas@gmail.com>
2016-02-09 22:16:53 -0800

    NERSC templates including - remove WMS js support - updated instructions for IDL and GRADS

--- src/Pydap-3.1.1/pydap/wsgi/template/data/+dot+static/js/html_form.js
+++ src/Pydap-3.1.1/pydap/wsgi/template/data/+dot+static/js/html_form.js
@@ -25,56 +25,56 @@ $(document).ready(function(){
     // Activate tabs.
     $("#tabs").tabs();
 
-    // Add map.
-    var map = new OpenLayers.Map('map');
-    map.addControl( new OpenLayers.Control.LayerSwitcher({
-            div: document.getElementById('layers'),
-            roundedCorner: false
-    }) );
-    var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
-             "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'},
-             {wrapDateLine: true});
-    map.addLayer(wms);
-    map.zoomToMaxExtent();
-    map.zoomIn();
-
-    // Add layers.
-    var location = document.location.href.split( /\?|#/ )[0];  // remove query-string and anchor
-    var dataset = location.substr(0, location.length-5);
-
-    var layers = [];
-    /*var kml = new OpenLayers.Layer.GML("KML", dataset + ".kml", {
-        format: OpenLayers.Format.KML,
-        formatOptions: {
-            extractStyles: true,
-            extractAttributes: true
-        }
-    });
-    layers.push(kml);*/
-
-    $.ajax({
-        type: "GET",
-        url: dataset + ".wms?REQUEST=GetCapabilities",
-        dataType: "xml",
-        success: function(xml) {
-            $(xml).find('Layer > Layer').each(function() {
-                var name = $(this).find('Name').text();
-                var title = $(this).find('Title').text();
-                layer = new OpenLayers.Layer.WMS( title, dataset + ".wms",
-                    {layers: name, TRANSPARENT: true},
-                    {isBaseLayer: false}
-                );
-                layer.setOpacity(0.8);
-                layer.setVisibility(false);
-                layers.push(layer);
-            });
-            if (layers.length > 0) {
-                map.addLayers(layers);
-                $("#wms").prepend("<h2>Visualizing data</h2>");
-            } else {
-                $("#tabs").tabs('disable', 1);
-                $("#wms").prepend("<h2>No qualifying data available</h2><p>Sorry, this dataset doesn't have any variables that can be represented as maps.</p>");
-            }
-        }
-    });
+//    // Add map.
+//    var map = new OpenLayers.Map('map');
+//    map.addControl( new OpenLayers.Control.LayerSwitcher({
+//            div: document.getElementById('layers'),
+//            roundedCorner: false
+//    }) );
+//    var wms = new OpenLayers.Layer.WMS( "OpenLayers WMS",
+//             "http://labs.metacarta.com/wms/vmap0", {layers: 'basic'},
+//             {wrapDateLine: true});
+//    map.addLayer(wms);
+//    map.zoomToMaxExtent();
+//    map.zoomIn();
+//
+//    // Add layers.
+//    var location = document.location.href.split( /\?|#/ )[0];  // remove query-string and anchor
+//    var dataset = location.substr(0, location.length-5);
+//
+//    var layers = [];
+//    /*var kml = new OpenLayers.Layer.GML("KML", dataset + ".kml", {
+//        format: OpenLayers.Format.KML,
+//        formatOptions: {
+//            extractStyles: true,
+//            extractAttributes: true
+//        }
+//    });
+//    layers.push(kml);*/
+//
+//    $.ajax({
+//        type: "GET",
+//        url: dataset + ".wms?REQUEST=GetCapabilities",
+//        dataType: "xml",
+//        success: function(xml) {
+//            $(xml).find('Layer > Layer').each(function() {
+//                var name = $(this).find('Name').text();
+//                var title = $(this).find('Title').text();
+//                layer = new OpenLayers.Layer.WMS( title, dataset + ".wms",
+//                    {layers: name, TRANSPARENT: true},
+//                    {isBaseLayer: false}
+//                );
+//                layer.setOpacity(0.8);
+//                layer.setVisibility(false);
+//                layers.push(layer);
+//            });
+//            if (layers.length > 0) {
+//                map.addLayers(layers);
+//                $("#wms").prepend("<h2>Visualizing data</h2>");
+//            } else {
+//                $("#tabs").tabs('disable', 1);
+//                $("#wms").prepend("<h2>No qualifying data available</h2><p>Sorry, this dataset doesn't have any variables that can be represented as maps.</p>");
+//            }
+//        }
+//    });
 });
